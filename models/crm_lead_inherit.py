e030794068e5785f6718c0c610910ae37ffe9cd5
bluemix <a.bluemix@gmail.com>
2024-11-10 14:23:35 +0100

    * Migrate the data that might already exist in `res.partner` to `crm.lead`

--- models/crm_lead_inherit.py
+++ models/crm_lead_inherit.py
@@ -1,8 +1,17 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class CrmLead(models.Model):
     """Extend `crm.lead` for additional information"""
 
     _inherit = "crm.lead"
-    linkedin_handle = fields.Char(string="LinkedIN Handle")
+    linkedin_handle = fields.Char(string="LinkedIN Handle",
+                                  compute="_compute_linkedin_handle",
+                                  store=True, )
+
+    @api.depends('partner_id.linkedin_handle')
+    def _compute_linkedin_handle(self):
+        """ used to migrate data from `res.partner.linkedin_handle`"""
+        for rec in self:
+            rec.linkedin_handle = rec.partner_id.linkedin_handle
+
